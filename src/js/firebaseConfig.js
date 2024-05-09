d5720e720182c2712bf21b8c507e3b8f5708162c
Benedicte Naess <144252770+benedictenaess@users.noreply.github.com>
2024-05-09 17:21:15 +0200

    user authentication works

---
+++ src/js/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+	apiKey: "AIzaSyDymJHoVfRsAGr7omzgFP2QM_RB7vpnSUQ",
+	authDomain: "final-project-5aec9.firebaseapp.com",
+	projectId: "final-project-5aec9",
+	storageBucket: "final-project-5aec9.appspot.com",
+	messagingSenderId: "1072862309911",
+	appId: "1:1072862309911:web:92268885e2fb57358bb652"
+  };
+
+  export default firebaseConfig;
