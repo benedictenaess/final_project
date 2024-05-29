1981eaf89892aca74db2ad455edf1b7cebaada10
Benedicte Naess <144252770+benedictenaess@users.noreply.github.com>
2024-05-29 11:33:25 +0200

    Server fix

--- server2.mjs
+++ server2.mjs
@@ -5,6 +5,14 @@ import 'dotenv/config';
 
 const PORT = 3000;
 const app = express();
+const requestOptions = {
+    method: 'GET', 
+    headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+    },
+    mode: 'cors',
+};
 app.use(cors());
 
 app.listen(PORT, ()=>{
@@ -15,7 +23,7 @@ const {API_KEY} = process.env
 
 app.get('/', async (req, res)=>{
 	try {
-		const response = await fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}`);
+		const response = await fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}`, requestOptions);
 		const data = await response.json();
 		const allGenres = data.genres;
 		res.json(allGenres);
